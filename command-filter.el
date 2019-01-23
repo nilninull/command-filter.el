3e37032549187fc85759e345f63de20fd7f880d3
nilninull <nilninull@gmail.com>
2019-01-23 15:47:17 +0900

    Update documents and add some macros

--- command-filter.el
+++ command-filter.el
@@ -1,6 +1,6 @@
 ;;; command-filter.el --- simple utilty macro for making text filter by unix command  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2015  nilninull
+;; Copyright (C) 2015, 2019  nilninull
 
 ;; Author: nilninull <nilninull@gmail.com>
 ;; Keywords: convenience
@@ -19,21 +19,79 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
+;; Sometimes, I would like to apply unix commands to Emacs buffers and regions.
+;;
+;; Emacs has `shell-command-on-region', but using a long command over
+;; and over is troublesome.
+;;
+;; That's why I wrote this program.
+;;
+;; This filter program affects the region when the region is active,
+;; and the entire buffer when the region is inactive.
+;;
+;;; Examples
+;; ;; Define filter commands like these.
+;;
+;; (define-command-filter test-filter-1-1 ("sed" "s/.*/\\U&/"))
+;;
+;; ;; You can specify multiple commands like shell pipes behavior.
+;;
+;; (define-command-filter test-filter-multi
+;;   ("awk" "-F	" "{print $3}")
+;;   ("sed" "s/.*/\\u&/"))
+;;
+;; ;; If you use only one command, you can omit parentheses.
+;;
+;; (define-command-filter test-filter-2-1 "awk" "{print toupper($0)}")
+;;
+;; ;; You can use multiline arguments.
+;;
+;; (define-command-filter test-filter-3-1 "python" "-c" "
+;; import sys;
+;; for line in sys.stdin:
+;;     print(line.upper(), end='')
+;; ")
+;;
+;; ;; And some other language examples.
+;;
+;; (define-command-filter test-filter-4-1 ("ruby" "-p" "-e" "$_.upcase!"))
+;;
+;; (define-command-filter test-filter-5-1 ("ghc" "-e" "interact (map Data.Char.toUpper)"))
+;;
+;; ;; There are special macros for some commands.
+;;
+;; (define-sed-filter test-filter-1-2 "s/.*/\\L&/")
+;;
+;; (define-awk-filter test-filter-2-2 "{print tolower($0)}")
+;;
+;; (define-python-filter test-filter-3-2 "-c" "
+;; import sys;
+;; for line in sys.stdin:
+;;     print(line.lower(), end='')
+;; ")
+;;
+;; (define-ruby-filter test-filter-4-2 "-p" "-e" "$_.downcase!")
+;;
+;; (define-ghc-filter test-filter-5-2 "-e" "interact (map Data.Char.toLower)")
 ;;
 
 ;;; Code:
 
 (defmacro define-command-filter (name &rest cmd-series)
-  "
-  (define-command-filter count-numbers
-    (\"sed\" \"s/[^0-9]\\+/ /g\")
-    (\"wc\" \"-w\"))
-"
+  "Define new command filter.
+
+NAME used by filter name like command-filter-NAME
+CMD-SERIES is a list of shell command lists or shell command lists.
+
+If you want to see an example, please read the comment of the program."
+
   (when (stringp (car cmd-series))
     (setq cmd-series (list cmd-series)))
   `(defun ,(intern (format "command-filter-%s" name)) ()
-     "This function was made by `define-command-filter'"
+     "This filter program affects the region when the region is active,
+and the entire buffer when the region is inactive.
+
+This function was made by `define-command-filter' macro."
      (interactive)
      (let ((temp-files (cl-loop repeat ,(1+ (length cmd-series))
                                 collect (make-temp-file "command-filter-")))
@@ -68,12 +126,39 @@
            (delete-file file))))))
 
 (defmacro define-sed-filter (name &rest options)
-  ""
-  `(define-command-filter ,name ("sed" ,@options)))
+  "Define sed filter command.
+
+NAME used by filter name like command-filter-NAME
+OPTIONS are passed to the program."
+  `(define-command-filter ,name "sed" ,@options))
 
 (defmacro define-awk-filter (name &rest options)
-  ""
-  `(define-command-filter ,name ("awk" ,@options)))
+  "Define awk filter command.
+
+NAME used by filter name like command-filter-NAME
+OPTIONS are passed to the program."
+  `(define-command-filter ,name "awk" ,@options))
+
+(defmacro define-python-filter (name &rest options)
+  "Define python filter command.
+
+NAME used by filter name like command-filter-NAME
+OPTIONS are passed to the program."
+  `(define-command-filter ,name "python" ,@options))
+
+(defmacro define-ruby-filter (name &rest options)
+  "Define ruby filter command.
+
+NAME used by filter name like command-filter-NAME
+OPTIONS are passed to the program."
+  `(define-command-filter ,name "ruby" ,@options))
+
+(defmacro define-ghc-filter (name &rest options)
+  "Define ghc filter command.
+
+NAME used by filter name like command-filter-NAME
+OPTIONS are passed to the program."
+  `(define-command-filter ,name "ghc" ,@options))
 
 (provide 'command-filter)
 ;;; command-filter.el ends here
